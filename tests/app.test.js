898cab4c6feac467ce60d0305ed89ac8c5908412
Devon Lumsden <devonlumsden@mail.com>
2019-04-18 18:32:02 +1200

    add inital test

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,9 @@ import {shallow} from 'enzyme'
 import App from '../client/components/App'
 
 test('<App />', () => {
-  const expected = 'React development has begun!'
+  const expected = 'MicroBlog Project'
   const wrapper = shallow(<App />)
-  expect(wrapper.text()).toBe(expected)
+
+  const actual = wrapper.find('h1')
+  expect(actual.text()).toBe(expected)
 })
