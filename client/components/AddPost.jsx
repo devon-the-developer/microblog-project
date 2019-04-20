f43265a242aa32725a32dd5020b072f822e70cd5
Devon Lumsden <devonlumsden@mail.com>
2019-04-20 13:36:24 +1200

    add handleChange

--- client/components/AddPost.jsx
+++ client/components/AddPost.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
 
 export class AddPost extends React.Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            postTitle: null,
+            postTags: null,
+            postContent: null
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]:value
+        })
+        console.log(event.target.name)
+    }
+
     render() {
         return (
             <div>
                 <form>
-                    <strong>Title: </strong>
-                    <input type='text' name='postTitle'></input>
+                    <label>Title: </label>
+                    <input type='text' name='postTitle' onChange={this.handleChange}></input>
                     <br />
-                    <strong>Tags: </strong>
-                    <input type='text' name='postTags'></input>
+                    <label>Tags: </label>
+                    <input type='text' name='postTags' onChange={this.handleChange}></input>
                     <br />
-                    <strong>Content: </strong>
-                    <input type='text' name='postContent' height='200px'></input>
+                    <label>Content: </label>
+                    <input type='text' name='postContent' height='200px' onChange={this.handleChange}></input>
                     <br />
                     <button>Submit</button>
                 </form>
+                <p>{this.state.postTitle} {this.state.postContent}</p>
             </div>
         )
     }
