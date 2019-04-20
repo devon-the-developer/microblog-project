a587713f3132560d7545cecd85d4f6169f34ea52
Devon Lumsden <devonlumsden@mail.com>
2019-04-21 00:02:02 +1200

    add hash routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 import { AddPost } from './AddPost';
 import { RandomPosts } from './RandomPosts'
 
@@ -44,12 +45,20 @@ export default class App extends React.Component {
 
     }
     return (
-      <div style={basicStyle}>
-      <h1>MicroBlog Project</h1>
-      <a href='#'>Add Post</a>
-      <RandomPosts value={this.state} />
-      <AddPost />
-      </div>
+      <Router>
+        <div style={basicStyle}>
+          <h1><strong>MicroBlog Project</strong></h1>
+          <div style={{display:'inline-block'}}>
+            <Link to='/'>Home</Link> <Link to='/addpost'>Add Post</Link>
+          </div>
+          <br />
+          <br />
+          <div>
+            <Route exact path='/' render={(props) => <RandomPosts value={this.state} />} />
+            <Route path='/addpost' component={AddPost} />
+          </div>
+        </div>
+      </Router>
     )
   }
 }
