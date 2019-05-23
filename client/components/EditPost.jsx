60977e8df4a46ed320e7badd4924ba42e9077d85
Devon Lumsden <devonlumsden@mail.com>
2019-05-23 14:30:44 +1200

    change local state to send

--- client/components/EditPost.jsx
+++ client/components/EditPost.jsx
@@ -5,11 +5,10 @@ export class EditPost extends React.Component {
         super(props)
         this.state = {
             posts: [],
-            currentId: null,
-            editPostTothis: {
-                name: null,
-                tags: null,
-                content: null}
+            currentId: null, 
+            nameChangeTo: null,
+            tagsChangeTo: null,
+            contentChangeTo: null
         }
     }
 
@@ -25,31 +24,34 @@ export class EditPost extends React.Component {
     }
 
     handleChange = (event) => {
-        console.log(event)
+        console.log(event.target.name)
         this.setState({
-            editPostTothis: {
                 [event.target.name]: event.target.value
-            }
         })
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('object i want to send: ', this.state.currentId, this.state.nameChangeTo, this.state.tagsChangeTo, this.state.contentChangeTo)
     }
 
     render(){
-        console.log(this.state.editPostTothis)
-        const currentPost = this.state.posts.find(post => post.id == this.state.currentId) || '' 
-        console.log('currentPost: ', currentPost)
+        const currentPost = this.state.posts.find(post => post.id == this.state.currentId) || ''
+        console.log(this.state)
         return (
             <div>
                 <p>You're on the edit page</p>
                 <form>
                     <h4>Edit Post</h4>
                     <label>Title: </label>
-                    <input type='text' name='name' onChange={this.handleChange} defaultValue={currentPost.name}></input>
+                    <input type='text' name='nameChangeTo' onChange={this.handleChange} defaultValue={currentPost.name}></input>
                     <br />
                     <label>Tags: </label>
-                    <input type='text' name='tags' onChange={this.handleChange} defaultValue={currentPost.tags}></input>
+                    <input type='text' name='tagsChangeTo' onChange={this.handleChange} defaultValue={currentPost.tags}></input>
                     <br />
                     <label>Content: </label>
-                    <textarea type='text' name='content' onChange={this.handleChange} defaultValue={currentPost.content} rows='50' cols='100'></textarea>
+                    <textarea type='text' name='contentChangeTo' onChange={this.handleChange} defaultValue={currentPost.content} rows='50' cols='100'></textarea>
                     <br />
                     <button onClick={this.handleSubmit}>Save Changes</button>
                 </form>
