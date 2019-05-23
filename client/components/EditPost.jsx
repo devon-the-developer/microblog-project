a221c802403ddf45c428d925405e00e8a0ca72f1
Devon Lumsden <devonlumsden@mail.com>
2019-05-24 10:10:48 +1200

    edit post redirects to main page

--- client/components/EditPost.jsx
+++ client/components/EditPost.jsx
@@ -42,6 +42,7 @@ export class EditPost extends React.Component {
             content: this.state.contentChangeTo
         }
         editPost(editObject)
+        this.props.history.push('/')
     }
 
     render(){
